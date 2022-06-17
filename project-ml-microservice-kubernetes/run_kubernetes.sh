d9ffb21c4d79595820d4265008288b6e27f2ea67
colmoc <colmoconchuir@gmail.com>
2022-06-17 09:57:58 +0000

    upload

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=colmoconchuir/houseimg:v1.0.1
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl create deploy python-housek8s --image=$dockerpath
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+kubectl get deployment
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/python-housek8s-55dfc4bfff-9ccv8 --address 0.0.0.0 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=colmoconchuir/houseimg:v1.0.1
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag house $dockerpath
+docker login
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
